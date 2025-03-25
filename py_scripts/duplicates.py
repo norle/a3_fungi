241d5a9f42c24da615201694a16cfb9e1cb282a2
Reinis <s233201@dtu.dk>
2025-03-25 11:15:09 +0100

    pre mod dm dupliacate adding

--- py_scripts/duplicates.py
+++ py_scripts/duplicates.py
@@ -2,11 +2,7 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from numba import jit
-
-def clean_sequence(seq):
-    """Remove gap characters and X from sequence"""
-    return seq.replace('-', '').replace('X', '')
-
+import re
 
 @jit(nopython=True)
 def fill_distance_matrix(n_total, idx_map_unique1, idx_map_unique2, mat_dist):
@@ -21,7 +17,7 @@ def fill_distance_matrix(n_total, idx_map_unique1, idx_map_unique2, mat_dist):
             full_mat[j, i] = dist  # Mirror
     return full_mat
 
-def manage_duplicates(in_fasta, unique_fasta, mat_dist):
+def manage_duplicates(in_fasta, unique_fasta, mat_dist, log_file):
     """
     Fill in distances for duplicate sequences in the distance matrix.
     
@@ -29,82 +25,63 @@ def manage_duplicates(in_fasta, unique_fasta, mat_dist):
         in_fasta (str): Path to original FASTA file with duplicates
         unique_fasta (str): Path to FASTA file with duplicates removed
         mat_dist (numpy.ndarray): Distance matrix for unique sequences
+        log_file (str): Path to IQ-TREE log file containing duplicate information
     
     Returns:
         pandas.DataFrame: Updated distance matrix including duplicates
     """
-    # Create mapping dictionaries
-    orig_seqs = {}
-    unique_seqs = {}
-    dup_to_unique = {}
+    # Read the unique sequences to get the base mapping
+    unique_seqs = pd.read_csv(unique_fasta, sep='\s+', header=None, skiprows=1)
+    unique_headers = unique_seqs[0].tolist()
     
-    # Read original sequences
+    # Create duplicate mapping from log file
+    dup_to_unique = {}
+    with open(log_file, 'r') as f:
+        for line in f:
+            m = re.search(r"NOTE:\s+(\S+)\s+\(identical to\s+(\S+)\)", line)
+            if m:
+                dup, orig = m.groups()
+                dup_to_unique[dup] = orig
+    
+    # Add non-duplicate sequences to the mapping
+    for header in unique_headers:
+        dup_to_unique[header] = header
+    
+    # Read original headers from FASTA
+    headers = []
     with open(in_fasta, 'r') as f:
-        header = ""
         for line in f:
             if line.startswith('>'):
-                header = line.strip()[1:]
-            else:
-                orig_seqs[header] = clean_sequence(line.strip())
+                headers.append(line.strip()[1:])
     
-    unique_seqs = pd.read_csv(unique_fasta, sep='\s+', header=None, skiprows=1)
-    unique_seqs = {row[0]: clean_sequence(row[1]) for row in unique_seqs.itertuples(index=False)}
-
-    # Create mapping for cleaned sequences
-    for orig_header, orig_seq in orig_seqs.items():
-        found_match = False
-        for unique_header, unique_seq in unique_seqs.items():
-            if orig_seq == unique_seq:
-                dup_to_unique[orig_header] = unique_header
-                found_match = True
-                break
-        if not found_match:
-            print(f"No match found for sequence: {orig_header}")
-            print(f"Original sequence: {orig_seq[:50]}...")  # Print first 50 chars
-            print("Available unique sequences:")
-            for k, v in unique_seqs.items():
-                print(f"{k}: {v[:50]}...")
-            raise ValueError(f"Could not find unique sequence match for {orig_header}")
+    # Create index mappings for numba
+    unique_header_to_idx = {h: i for i, h in enumerate(unique_headers)}
+    idx_map_unique1 = np.array([unique_header_to_idx[dup_to_unique[h]] for h in headers])
+    idx_map_unique2 = np.array([unique_header_to_idx[dup_to_unique[h]] for h in headers])
     
-    # Create new matrix with space for duplicates
-    n_total = len(orig_seqs)
-    #print(dup_to_unique)
-    # Create index mapping
-    headers = list(orig_seqs.keys())
-    header_to_idx = {h: i for i, h in enumerate(headers)}
-    
-    # Prepare index mappings for numba
-    idx_map_unique1 = np.array([list(unique_seqs.keys()).index(dup_to_unique[h]) for h in headers])
-    idx_map_unique2 = np.array([list(unique_seqs.keys()).index(dup_to_unique[h]) for h in headers])
-    
-    # Use accelerated function
+    # Generate full matrix
+    n_total = len(headers)
     full_mat = fill_distance_matrix(n_total, idx_map_unique1, idx_map_unique2, mat_dist)
     
     # Create DataFrame with accessions
     df_full = pd.DataFrame(full_mat, index=headers, columns=headers)
-    # Reset index to make accessions a column
     df_full.reset_index(inplace=True)
     df_full.rename(columns={'index': ''}, inplace=True)
     
     return df_full
 
 if __name__ == '__main__':
-    import pandas as pd
-    import numpy as np
-    
-    in_fasta = '/work3/s233201/enzyme_out_3/ACO2.fasta'
-    unique_fasta = '/work3/s233201/enzyme_out_3/enzyme_trees/ACO2/tree_iq_multi_LGI.uniqueseq.phy'
-    mat_dist_name = '/work3/s233201/enzyme_out_3/enzyme_trees/ACO2/tree_iq_multi_LGI.mldist'
+    gene_names = ["LYS20", "ACO2", "LYS4", "LYS12", "ARO8", "LYS2", "LYS9", "LYS1"]
+    for gene_name in gene_names:
+        in_fasta = f'/work3/s233201/enzyme_out_3/{gene_name}.fasta'
+        unique_fasta = f'/work3/s233201/enzyme_out_3/enzyme_trees/{gene_name}/tree_iq_multi_LGI.uniqueseq.phy'
+        mat_dist_name = f'/work3/s233201/enzyme_out_3/enzyme_trees/{gene_name}/tree_iq_multi_LGI.mldist'
+        log_file = f'/work3/s233201/enzyme_out_3/enzyme_trees/{gene_name}/tree_iq_multi_LGI.log'
 
-    mat_dist= pd.read_csv(mat_dist_name, 
-                                sep='\s+',
-                                header=None,
-                                skiprows=1)
-
-    mat_dist = mat_dist.iloc[:, 1:].to_numpy()
-    full_mat = manage_duplicates(in_fasta, unique_fasta, mat_dist)
-
-    output_path = '/zhome/85/8/203063/a3_fungi/full_dist_mats/full_mat_ACO2.csv'
-    full_mat.to_csv(output_path, sep=' ', index=False)
-
-    print(full_mat.shape)
+        mat_dist = pd.read_csv(mat_dist_name, sep=r'\s+', header=None, skiprows=1)
+        mat_dist = mat_dist.iloc[:, 1:].to_numpy()
+        
+        full_mat = manage_duplicates(in_fasta, unique_fasta, mat_dist, log_file)
+        output_path = f'/zhome/85/8/203063/a3_fungi/full_dist_mats/full_mat_{gene_name}.csv'
+        full_mat.to_csv(output_path, sep=' ', index=False)
+        print(full_mat.shape)
